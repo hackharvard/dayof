a3c2bb97a752a531ab6760449fe15ec908e0c902
dseum22 <dseum22@gmail.com>
2023-06-29 17:23:32 +0400

    Fixed $ reference

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-  import Applications from '../lib/sections/Applications.svelte'
-  import Faq from '../lib/sections/Faq.svelte'
-  import Landing from '../lib/sections/Landing.svelte'
-  import Speakers from '../lib/sections/Speakers.svelte'
-  import Tracks from '../lib/sections/Tracks.svelte'
-  import Tracksprizes from '../lib/sections/TracksPrizes.svelte'
-  import Welcome from '../lib/sections/Welcome.svelte'
-  import Sponsors from '../lib/sections/Sponsors.svelte'
+  import Applications from '$lib/sections/Applications.svelte'
+  import Faq from '$lib/sections/Faq.svelte'
+  import Landing from '$lib/sections/Landing.svelte'
+  import Speakers from '$lib/sections/Speakers.svelte'
+  import Tracks from '$lib/sections/Tracks.svelte'
+  import Tracksprizes from '$lib/sections/TracksPrizes.svelte'
+  import Welcome from '$lib/sections/Welcome.svelte'
+  import Sponsors from '$lib/sections/Sponsors.svelte'
 </script>
 
 <main class="mt-20">
