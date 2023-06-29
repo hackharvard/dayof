a5222387cc6a44e097c154c226022c4076b097ff
dseum22 <dseum22@gmail.com>
2023-06-29 16:33:20 +0400

    Created basic marquee banner

---
+++ src/lib/components/Marquee.svelte
@@ -0,0 +1,35 @@
+<div
+  class="flex uppercase text-2xl overflow-hidden w-full select-none gap-10 border-t-4 border-b-4 border-black py-2 font-bold"
+>
+  <div class="flex marquee min-w-full gap-10 justify-around shrink-0">
+    <span>Apply today!</span>
+    <span>HackHarvard 2023!</span>
+    <span>Apply today!</span>
+    <span>HackHarvard 2023!</span>
+  </div>
+  <div class="flex marquee min-w-full gap-10 justify-around shrink-0">
+    <span>Apply today!</span>
+    <span>HackHarvard 2023!</span>
+    <span>Apply today!</span>
+    <span>HackHarvard 2023!</span>
+  </div>
+</div>
+
+<style>
+  .marquee {
+    animation: marquee 10s linear infinite;
+  }
+  @keyframes marquee {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(calc(-100% - 2.5rem));
+    }
+  }
+  @media (prefers-reduced-motion: reduce) {
+    .marquee {
+      animation-play-state: paused;
+    }
+  }
+</style>
