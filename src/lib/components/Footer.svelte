caed42e71ef6b1b61c78ad4e140a32324b90aeba
dseum22 <dseum22@gmail.com>
2023-07-06 11:58:13 +0400

    Dark mode to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="flex items-center justify-center py-6">
+<footer class="flex items-center justify-center py-6 dark:bg-secondary">
   <span>&copy; {new Date().getFullYear()} HackHarvard</span>
 </footer>
