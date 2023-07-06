e648b7c1f8965a2703e3e287beafc9a35b96f6be
dseum22 <dseum22@gmail.com>
2023-07-06 12:12:41 +0400

    Formatting

--- src/lib/sections/Faq.svelte
+++ src/lib/sections/Faq.svelte
@@ -1,7 +1,7 @@
 <div class="p-dynamic">
   <div>
-    <h1 class="text-red-700 text-5xl font-bold">FAQ</h1>
-    <p class="text-blue-700 mt-5">What if my question is not here?</p>
+    <h1 class="text-5xl font-bold text-red-700">FAQ</h1>
+    <p class="mt-5 text-blue-700">What if my question is not here?</p>
     <p>
       Please email us at <a class="link" href="mailto:team@hackharvard.io">team@hackharvard.io</a> and
       we'll get back to you ASAP!
@@ -9,31 +9,31 @@
   </div>
   <div class="mt-dynamic grid grid-cols-2 gap-6">
     <div class="col-span-1">
-      <h3 class="text-red-700 text-3xl font-bold">GENERAL</h3>
-      <h1 class="text-blue-700 mt-3">What if I've never hacked before?</h1>
+      <h3 class="text-3xl font-bold text-red-700">GENERAL</h3>
+      <h1 class="mt-3 text-blue-700">What if I've never hacked before?</h1>
       <p>
         That's ok! In fact, you've found the perfect place and time to pick up new skills and
         concepts. There will be mentors, workshops, and tech talks to help ease you through the
         hackathon experience. Applications are not filtered based on experience. First time hackers
         are also eligible for the first-time hacker prize!
       </p>
-      <h1 class="text-blue-700 mt-3">When is registration?</h1>
+      <h1 class="mt-3 text-blue-700">When is registration?</h1>
       <p>Early applications will be due XX. Round 2 applications will be due XX.</p>
-      <h1 class="text-blue-700 mt-3">Can I attend?</h1>
+      <h1 class="mt-3 text-blue-700">Can I attend?</h1>
       <p>
         As long as you are an undergraduate student at any college in the world and are 18 or older,
         you are invited to apply to HackHarvard!
       </p>
     </div>
     <div class="col-span-1 ml-5">
-      <h3 class="text-red-700 text-3xl font-bold">TEAMS</h3>
-      <h1 class="text-blue-700 mt-3">What if I don't have a team?</h1>
+      <h3 class="text-3xl font-bold text-red-700">TEAMS</h3>
+      <h1 class="mt-3 text-blue-700">What if I don't have a team?</h1>
       <p>
         If you registered as an individual and do not have a team yet, don’t sweat it. There will be
         several online events and channels geared towards facilitating team formation and project
         ideation. We will help you find people to work with!
       </p>
-      <h1 class="text-blue-700 mt-3">How do teams work?</h1>
+      <h1 class="mt-3 text-blue-700">How do teams work?</h1>
       <p>
         Each team consists of a maximum of four members. You can create a team and designate
         teammates during registration, or you can register as an individual. All teammates must
