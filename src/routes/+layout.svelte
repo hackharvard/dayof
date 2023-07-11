2d6e06cbddd973f4f3054d0fc1d47d3cd9592336
Yuen Ler Chow <lerchow@gmail.com>
2023-07-11 01:38:44 -0400

    nav bar disappearing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,35 @@
 <script>
+  import { onMount } from 'svelte'
+
   import '../app.css'
   import Footer from '$lib/components/Footer.svelte'
   import Nav from '$lib/components/Nav.svelte'
+  import { slide } from 'svelte/transition'
+
+  let prevScrollPos = 0
+  let showNav = true
+
+  function handleScroll() {
+    const currentScrollPos = window.pageYOffset
+    showNav = prevScrollPos > currentScrollPos
+    prevScrollPos = currentScrollPos
+  }
+
+  onMount(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll)
+    }
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }
+  })
 </script>
 
-<Nav />
+{#if showNav}
+  <Nav />
+{/if}
+
 <slot />
 <Footer />
