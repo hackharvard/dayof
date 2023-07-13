1da84b43d18adb8ebcc1903c24e409ef5cc165b5
Yuen Ler Chow <lerchow@gmail.com>
2023-07-13 01:35:36 -0400

    logo with text

--- src/lib/components/Brand.svelte
+++ src/lib/components/Brand.svelte
@@ -3,7 +3,7 @@
     class="inline-flex items-center gap-2 rounded-md border border-gray-200 p-2 dark:border-secondary"
   >
     <a class="shrink-0" href="/#home">
-      <img class="h-8 w-8" src="/favicon.svg" alt="" />
+      <img class="h-10" src="/logo.svg" alt="" />
     </a>
   </div>
 </div>
