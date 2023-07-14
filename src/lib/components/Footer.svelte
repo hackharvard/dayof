aa6ca987532052643e9d1500dc8fbbf33d842bd2
Yuen Ler Chow <lerchow@gmail.com>
2023-07-13 23:53:29 -0400

    move social icons to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,104 @@
-<footer class="flex items-center justify-center bg-purple-100 py-6 dark:bg-secondary">
-  <span>&copy; {new Date().getFullYear()} HackHarvard</span>
+<footer>
+  <div class="bg-black">
+    <!-- Social media banner -->
+    <div class="p-dynamic flex w-full justify-center">
+      <div class="flex items-center gap-3">
+        <a
+          class="rounded-md border-2 border-white p-2"
+          href="https://www.instagram.com/hackharvardcollege/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            class="h-8 w-8 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
+          </svg>
+        </a>
+        <a
+          class="rounded-md border-2 border-white p-2"
+          href="https://www.facebook.com/hackharvard/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            class="h-8 w-8 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+          </svg>
+        </a>
+
+        <a
+          class="rounded-md border-2 border-white p-2"
+          href="https://www.linkedin.com/company/hackharvardcollege/mycompany/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            class="h-8 w-8 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+            />
+            <rect x="2" y="9" width="4" height="12" />
+            <circle cx="4" cy="4" r="2" />
+          </svg>
+        </a>
+        <a
+          class="rounded-md border-2 border-white p-2"
+          href="mailto:team@hackharvard.io"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            class="h-8 w-8 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+            <polyline points="22,6 12,13 2,6" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="flex justify-center text-white">
+      <span>&copy; {new Date().getFullYear()} HackHarvard</span>
+    </div>
+  </div>
 </footer>
--- src/lib/sections/Landing.svelte
+++ src/lib/sections/Landing.svelte
@@ -45,7 +45,7 @@
 </script>
 
 <div class="bg-landing-light dark:bg-landing-dark" id="home">
-  <div class="p-dynamic grow lg:grid lg:grid-cols-2">
+  <div class="px-dynamic grow pb-5 lg:grid lg:grid-cols-2">
     <div class="flex items-center justify-center">
       <div class="w-60 max-w-xl lg:w-full">
         <img
@@ -89,115 +89,14 @@
             </div>
           </div>
         </div>
+        <div class="mt-10 text-3xl font-bold uppercase text-white">
+          Applications open in August!
+        </div>
       </div>
     </div>
   </div>
   <div class="pb-8">
     <Marquee theme="light" />
-  </div>
-</div>
-<!-- Social media banner -->
-<div class="p-dynamic grid w-full grid-cols-1 gap-6 dark:bg-black lg:grid-cols-2">
-  <div class="flex h-full items-center justify-center">
-    <div class="text-3xl font-bold uppercase text-secondary dark:text-white">
-      Applications open in August!
-    </div>
-  </div>
-  <div class="flex h-full items-center justify-center">
-    <div class="flex items-center gap-3">
-      <a
-        class="rounded-md border-2 border-black p-2 dark:border-white"
-        href="https://www.instagram.com/hackharvardcollege/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <svg
-          class="h-8 w-8 text-black dark:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
-          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-          <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-        </svg>
-      </a>
-      <a
-        class="rounded-md border-2 border-black p-2 dark:border-white"
-        href="https://www.facebook.com/hackharvard/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <svg
-          class="h-8 w-8 text-black dark:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-        </svg>
-      </a>
-
-      <a
-        class="rounded-md border-2 border-black p-2 dark:border-white"
-        href="https://www.linkedin.com/company/hackharvardcollege/mycompany/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <svg
-          class="h-8 w-8 text-black dark:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path
-            d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
-          />
-          <rect x="2" y="9" width="4" height="12" />
-          <circle cx="4" cy="4" r="2" />
-        </svg>
-      </a>
-      <a
-        class="rounded-md border-2 border-black p-2 dark:border-white"
-        href="mailto:team@hackharvard.io"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <svg
-          class="h-8 w-8 text-black dark:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-          <polyline points="22,6 12,13 2,6" />
-        </svg>
-      </a>
-    </div>
   </div>
 </div>
 
