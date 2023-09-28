248c57c16d0c073d1dc65c6311b7babf907c866b
Yuen Ler Chow <lerchow@gmail.com>
2023-09-28 11:54:24 -0400

    mathworks

--- src/lib/sections/Sponsors.svelte
+++ src/lib/sections/Sponsors.svelte
@@ -45,6 +45,17 @@
       </a>
 
       <a
+        href="https://www.mathworks.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="dark:rounded-2xl dark:bg-white sm:p-4 md:p-8 lg:p-12"
+      >
+        <div class="flex h-full items-center">
+          <img src="images/sponsors/mathworks.jpeg" alt="Mathworks" />
+        </div>
+      </a>
+
+      <a
         href="https://www.tensordock.com/"
         target="_blank"
         rel="noopener noreferrer"
@@ -84,7 +95,7 @@
         class="dark:rounded-2xl dark:bg-white sm:p-4 md:p-8 lg:p-12"
       >
         <div class="flex h-full items-center">
-          <img src="images/sponsors/incogni.png" alt="you.com" />
+          <img src="images/sponsors/incogni.png" alt="incogni" />
         </div>
       </a>
     </div>
