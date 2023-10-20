3a62684a329db57f85c22d89337231351fd7964e
LANCE BAE <LBAE@COLLEGE.HARVARD.EDU>
2023-10-20 15:43:04 -0400

    Updated tracks

--- src/lib/sections/Tracks.svelte
+++ src/lib/sections/Tracks.svelte
@@ -10,17 +10,17 @@
     },
     {
       img: 'images/tracks/space.jpg',
-      name: 'Earth and Space\n',
+      name: 'Earth and Space',
       desc: "Embark on a journey beyond our planet and explore the vastness of both Earth and Space in this hackathon track. From addressing environmental challenges on our home planet to pioneering new frontiers in space exploration, this track welcomes visionaries and problem solvers who are eager to make a significant impact on our world, both near and far. Whether you're interested in climate solutions, space technology, or protecting our planet, this hackathon is your launchpad to innovation."
     },
     {
       img: 'images/tracks/better.jpg',
       name: 'So You Think You Can Do It\nBetter',
-      desc: "Do you believe you can take existing products, services, or systems and make them infinitely better? If you're an advocate for improving the status quo and have innovative ideas to disrupt industries, this is your chance to shine. In the 'So You Think You Can Do It Better' Hackathon, you'll challenge the conventional and reimagine how things can be done. From upgrading everyday apps to reinventing established processes, bring your game-changing ideas to the table and prove that you can indeed do it better!"
+      desc: 'Do you believe you can take existing products, services, or systems and make them infinitely better? If you\'re an advocate for improving the status quo and have innovative ideas to disrupt industries, this is your chance to shine. In the "So You Think You Can Do It Better" Hackathon, you\'ll challenge the conventional and reimagine how things can be done. From upgrading everyday apps to reinventing established processes, bring your game-changing ideas to the table and prove that you can indeed do it better!'
     },
     {
       img: 'images/tracks/efficiency.jpg',
-      name: 'Efficiency Boosters\n ',
+      name: 'Efficiency Boosters',
       desc: "Efficiency fuels progress in our fast-paced world. If you're a problem solver with a passion for streamlining processes and maximizing productivity, then the Efficiency Boosters Hackathon is the perfect platform for you. Join forces with like-minded individuals to develop innovative solutions that optimize workflows, enhance resource management, and boost overall efficiency across various domains. Your ingenuity could revolutionize industries, so let's hack for a more efficient future!"
     }
   ]
